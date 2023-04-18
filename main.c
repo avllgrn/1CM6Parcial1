3c9cea71253c8108582792cc12d025da56afc80c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2023-04-18 09:41:20 -0600

    Rectangulo con funciones con retorno y argumentos

--- main.c
+++ main.c
@@ -2,19 +2,25 @@
 #include <math.h>
 
 float sumaDosNumeros(float x, float y);
+float calculaAreaRectangulo(float b, float h);
+float calculaPerimetroRectangulo(float b, float h);
 
 int main(void){
-	float a, b, c;
+	float base, altura,area,perimetro;
 	//1.Pide datos
-	printf("Ingresa a ");
-	scanf("%f",&a);
-	printf("Ingresa b ");
-	scanf("%f",&b);
+	printf("Ingresa base ");
+	scanf("%f",&base);
+	printf("Ingresa altura ");
+	scanf("%f",&altura);
 
-	c=sumaDosNumeros(a,b);
+	area = calculaAreaRectangulo(base, altura);
+	perimetro = calculaPerimetroRectangulo(base, altura);
 
 	//3.Muestra resultado
-	printf("%f + %f = %f\n",a,b,c);
+	printf("base = %f\n",base);
+	printf("altura = %f\n",altura);
+	printf("area = %f\n",area);
+	printf("perimetro = %f\n",perimetro);
     return 0;
 }
 
@@ -23,4 +29,14 @@ float sumaDosNumeros(float x, float y){
     //2.Calcula formula
     z = x+y;
     return z;
+}
+float calculaAreaRectangulo(float b, float h){
+    float a;
+    a = b*h;//2.Calcula formula
+    return a;
+}
+float calculaPerimetroRectangulo(float b, float h){
+    float p;
+    p = 2*b + 2*h;//2.Calcula formula
+    return p;
 }
